44fc735ca924f1ca8158fb02a7ded285f10b650c
tariqjunaid <junaidtariq_91@yahoo.com>
2019-06-05 15:35:07 -0400

    Added create train

--- app/controllers/station_controller.rb
+++ app/controllers/station_controller.rb
@@ -1,3 +1,12 @@
 class StationController < Sinatra::Base
+  set :views, './app/views/stations'
 
+    get '/stations' do
+      @stations = Station.all
+      erb :index
+    end
+
+    get '/stations/new' do
+      erb :new
+    end
 end
--- app/controllers/train_controller.rb
+++ app/controllers/train_controller.rb
@@ -1,3 +1,23 @@
 class TrainController < Sinatra::Base
+  set :views, './app/views/trains'
 
+    get '/trains' do
+      @trains = Train.all
+      erb :index
+    end
+
+    get '/trains/new' do
+      erb :new
+    end
+
+    post '/trains' do
+      train = Train.create(line_color: params["line_color"], series: params["series"], cars: params["cars"], destination_name: params["destination_name"])
+      # binding.pry
+      redirect '/trains/#{train.id}'
+    end
+
+    get '/trains/:id' do
+      @train = Train.find(params[:id])
+      erb :show
+    end
 end
